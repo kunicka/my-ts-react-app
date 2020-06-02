71e032db6ff68ac2e74e0d41dc371ca613795939
kunicka <kunicka2000@gmail.com>
2020-06-02 10:53:29 +0200

    trial commit through VSC level

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <h1>I am a sexi header of this page</h1>
+            <h1>I am a sexi header of this page !</h1>
         </div>
     )
 }
